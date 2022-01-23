a5fcdf8c60ac9f01fc3fb75ce2b6f0f358ef51dc
Shekhar Ranjan <shekhar10feb@gmail.com>
2022-01-23 17:20:18 +0530

    Update quoteGenerator.js

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,4 +1,27 @@
-const arr = ["<q>Merciless criticism and independent thinking are the two necessary traits of revolutionary thinking.</q> -- <b>Martyr Bhagat Singh</b>", "<q>Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.</q> -- <b>Albert Einstein</b>", "<q>Freedom is not given - it is taken.</q> -- <b>Netaji Subhas Chandra Bose</b>", "<q>Take up one idea. Make that one idea your life, think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body be full of that idea, and just leave every other idea alone. This is the way to success.</q> -- <b>Swami Vivekananda</b>", "<q>Will play holi with blood if the country is in trouble.</q> -- <b>Ashfaqullah Khan</b>", "<q>All our dreams can come true if we have the courage to pursue them.</q> -- <b>Walt Disney</b>", "<q>So long as you do not achieve social liberty, whatever freedom is provided by the law is of now avail to you.</q> -- <b>BR Ambedkar</b>", "<q>There is no easy walk to freedom anywhere, and many of us will have to pass through the valley of the shadow of death again and again before we reach the mountaintop of our desires.</q> -- <b>Nelson Mandela</b>", "<q>You have to dream before your dreams can come true.</q> -- <b>APJ Abdul Kalam</b>", "<q>Don't see others doing better than you beat your own records everyday, because success is a fight between you and yourself.</q> -- <b>Chandra Shekhar Azad</b>","<q>Live as if you were to die tomorrow. Learn as if you were to live forever.</q> -- <b>Mahatma Gandhi</b>", "<q>One individual may die for an idea, but that idea will, after his death, incarnate itself in a thousand lives.</q> -- <b>Netaji Subhas Chandra Bose</b>"];
+const arr = ["Merciless criticism and independent thinking are the two necessary traits of revolutionary thinking.", 
+             " -- Martyr Bhagat Singh", 
+"Learn from yesterday, live for today, hope for tomorrow. The important thing is not to stop questioning.", 
+             " -- Albert Einstein", 
+             "You have to dream before your dreams can come true.", 
+             " -- APJ Abdul Kalam", 
+             "Freedom is not given - it is taken.", 
+             " -- Netaji Subhas Chandra Bose", 
+"Take up one idea. Make that one idea your life, think of it, dream of it, live on that idea. Let the brain, muscles, nerves, every part of your body be full of that idea, and just leave every other idea alone. This is the way to success.", 
+             " -- Swami Vivekananda", 
+"Will play holi with blood if the country is in trouble.", 
+             " -- Ashfaqullah Khan", 
+             "All our dreams can come true if we have the courage to pursue them.", 
+             " -- Walt Disney", 
+"So long as you do not achieve social liberty, whatever freedom is provided by the law is of now avail to you.", 
+             " -- BR Ambedkar", 
+"There is no easy walk to freedom anywhere, and many of us will have to pass through the valley of the shadow of death again and again before we reach the mountaintop of our desires.", 
+             " -- Nelson Mandela", 
+"Don't see others doing better than you beat your own records everyday, because success is a fight between you and yourself.", 
+             " -- Chandra Shekhar Azad",
+"Live as if you were to die tomorrow. Learn as if you were to live forever.", 
+             " -- Mahatma Gandhi", 
+"One individual may die for an idea, but that idea will, after his death, incarnate itself in a thousand lives.", 
+             " -- Netaji Subhas Chandra Bose"];
 
 // JavaScript window.location object can be used
 
@@ -28,6 +51,16 @@ document.querySelector("#refresh").addEventListener("click", function() {
     	      window.location.reload(true);
  });
 
+let x = Math.floor((Math.random() * 12) + 1);
+    
+function generatorFunction(x) {
+  if(x % 2 == 0){
+        // return `${x} + ${x + 1}`;
+      return document.querySelector(".quote").innerHTML = `<q>${arr[x]}</q><b>${arr[x + 1]}</b>`;
+    } else {
+        // return `${x - 1} + ${x}`;
+      return document.querySelector(".quote").innerHTML = `<q>${arr[x-1]}</q><b>${arr[x]}</b>`;
+    }
+}
 
-const x = Math.floor((Math.random() * 12) + 1);
-document.querySelector(".quote").innerHTML = arr[x];
+generatorFunction(x);
