847f56a921040ff5c4de768e8269dbd95b131262
Shekhar Ranjan <shekhar10feb@gmail.com>
2022-01-30 15:06:37 +0530

    Update quoteGenerator.js

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -51,7 +51,7 @@ document.querySelector("#refresh").addEventListener("click", function() {
     	      window.location.reload(true);  
  });
 
-let x = Math.floor(Math.random() * 25);
+let x = Math.floor(Math.random() * 24);
     
 function generatorFunction(x) {
   if(x % 2 == 0){
